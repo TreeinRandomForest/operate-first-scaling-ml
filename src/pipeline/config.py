a2ed0e306bd89930e23d19bbdaf1072a70e32d27
sanjay <treeinrandomforest@gmail.com>
2021-02-25 17:17:39 -0500

    skeleton pipeline for hyperparam grid

---
+++ src/pipeline/config.py
@@ -0,0 +1,7 @@
+import os
+
+S3_END_POINT = os.getenv('S3_END_POINT', 'http://minio-kubeflow.apps.cluster-9566.9566.example.opentlc.com')
+S3_ACCESS_ID = os.getenv('AWS_ACCESS_KEY_ID')
+S3_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+BASE_IMAGE = 'docker.io/pytorch/pytorch:1.0-cuda10.0-cudnn7-runtime'
---
+++ src/pipeline/utils.py
@@ -0,0 +1,8 @@
+import numpy as np
+import boto3
+
+def write_to_store():
+    pass
+
+def read_from_store():
+    pass
